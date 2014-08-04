f17b47f2c72127c84900bf736cb5effb657dc04b
Will Rogers <wahrogers@gmail.com>
2014-08-04 10:11:43 +0100

    Correct directories in start.sh.

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TW=wrtw.html
+cd $DIR
 
 if [[ ! -z $(git status --porcelain) ]] ; then
 	echo "Local changes; can't start."
 else
 	git pull origin master
 
-	firefox $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/$TW
+	firefox $TW
 fi
+
+cd -
