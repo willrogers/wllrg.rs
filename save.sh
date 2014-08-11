efbd7ea465c6fdbf7d75eb29cc34e2958936d067
Will Rogers <wahrogers@gmail.com>
2014-08-11 20:49:00 +0100

    Add shebang to save.sh.

--- save.sh
+++ save.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TW_NAME=wrtw.html
 cd $DIR
 x=$(git add $TW_NAME 2>&1)
 y=$(git commit -m "Standard update to TiddlyWiki." 2>&1)
 z=$(git push origin master 2>&1)
-echo "$x$y$z"
 zenity --info --text="$x$y$z"
 cd -
 
