6e0db55f7d68cbc84c302ccc16ed91a82c551b71
Will Rogers <wahrogers@gmail.com>
2015-08-01 13:22:08 +0100

    First attempt at Galleria for photos.

---
+++ js/photos.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    console.log('Loading galleria.');
+    Galleria.loadTheme('../galleria/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria');
+    console.log('Loaded galleria.');
+});
+
+
