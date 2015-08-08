d7a9cb9f6c970847e0ef08e72cfa0682d3d3a4a2
Will Rogers <wahrogers@gmail.com>
2015-08-08 12:26:37 +0100

    Remove Galleria.

--- js/photos.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function() {
-    console.log('Loading galleria.');
-    Galleria.loadTheme('../galleria/themes/classic/galleria.classic.min.js');
-    Galleria.run('.galleria');
-    console.log('Loaded galleria.');
-});
-
-
