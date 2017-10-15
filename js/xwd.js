d75f408c92831977c09d3e24204103bf2d412b32
Will Rogers <wahrogers@gmail.com>
2017-10-15 10:43:20 +0100

    First draft of js-based crossword.

---
+++ js/xwd.js
@@ -0,0 +1,133 @@
+/* Draw a crossword on the screen. */
+
+AC_SQUARES = 13;
+DN_SQUARES = 13;
+LINE_WIDTH = 1;
+
+
+BLACK_SQUARES = [[0, 0], [0, 2], [0, 4], [0, 6], [0, 8], [0, 10], [0, 12],
+                 [1, 8],
+                 [2, 0], [2, 2], [2, 4], [2, 6], [2, 8], [2, 10], [2, 12],
+                 [3, 4],
+                 [4, 0], [4, 1], [4, 2], [4, 4], [4, 6], [4, 8], [4, 9], [4, 10], [4, 12],
+                 [5, 6],
+                 [6, 0], [6, 2], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 10], [6, 12],
+                 [7, 6],
+                 [8, 0], [8, 2], [8, 3], [8, 4], [8, 6], [8, 8], [8, 10], [8, 11], [8, 12],
+                 [9, 8],
+                 [10, 0], [10, 2], [10, 4], [10, 6], [10, 8], [10, 10], [10, 12],
+                 [11, 4],
+                 [12, 0], [12, 2], [12, 4], [12, 6], [12, 8], [12, 10], [12, 12]]
+
+cellInArray = function(array, x, y) {
+    for (var k = 0; k < array.length; k++) {
+        if (array[k][0] === x && array[k][1] === y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+WHITE_SQUARES = []
+for (var i = 0; i < AC_SQUARES; i++) {
+    for (var j = 0; j < DN_SQUARES; j++) {
+        if (!cellInArray(BLACK_SQUARES, i, j)) {
+            WHITE_SQUARES.push([i, j]);
+        }
+    }
+}
+
+drawNumber = function(ctx, cellSize, number, x, y) {
+    ctx.font = '18px serif';
+    ctx.textBaseline = 'hanging';
+    ctx.fillText(number, cellSize * x + 3, cellSize * y + 3);
+}
+
+drawWhiteSquare = function(ctx, cellSize, x, y) {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(cellSize * x + 2, cellSize * y + 2, cellSize - 2, cellSize - 2);
+}
+
+drawGrid = function(ac_squares, dn_squares) {
+    var canvas = document.getElementById('xwd');
+    var ctx = canvas.getContext('2d');
+    canvas.width = 602;
+    canvas.height = 602;
+    var xpos = canvas.offsetLeft;
+    var ypos = canvas.offsetTop;
+    var cellSize = Math.floor(Math.min(canvas.width / AC_SQUARES, canvas.height / DN_SQUARES));
+    var width = cellSize * AC_SQUARES + 2;
+    var height = cellSize * DN_SQUARES + 2;
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.fillRect(0, 0, width, height);
+    /* Draw in the white squares. */
+    for (var i = 0; i < AC_SQUARES; i++) {
+        for (var j = 0; j < DN_SQUARES; j++) {
+            var drawWhite = true;
+            for (var k = 0; k < BLACK_SQUARES.length; k++) {
+                if (i === BLACK_SQUARES[k][0] && j === BLACK_SQUARES[k][1]) {
+                    drawWhite = false;
+                    break;
+                }
+            }
+            if (drawWhite) {
+                drawWhiteSquare(ctx, cellSize, i, j);
+            }
+        }
+    }
+    /* Collect clues and write in numbers */
+    ctx.fillStyle = 'black';
+    var clueNumber = 1;
+    var acClues = {};
+    var dnClues = {};
+    console.log(cellInArray(BLACK_SQUARES, 4, 1));
+    console.log(cellInArray(WHITE_SQUARES, 4, 1));
+    /* loop from right to left then top to bottom */
+    for (var j = 0; j < DN_SQUARES; j++) {
+        for (var i = 0; i < AC_SQUARES; i++) {
+            acrossCount = 0;
+            downCount = 0;
+            if (cellInArray(WHITE_SQUARES, i, j)) {
+                /* Start of across clue */
+                if (i === 0 || !cellInArray(WHITE_SQUARES, i - 1, j)) {
+                    acrossCount = 1;
+                    for (var l = i + 1; l < AC_SQUARES; l++) {
+                        if (cellInArray(WHITE_SQUARES, l, j)) {
+                            acrossCount += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (acrossCount > 1) {
+                        acClues[clueNumber] = [i, j, acrossCount];
+                    }
+                }
+                /* Start of down clue */
+                if (j === 0 || !cellInArray(WHITE_SQUARES, i, j - 1)) {
+                    downCount = 1;
+                    for (var l = j + 1; l < DN_SQUARES; l++) {
+                        if (cellInArray(WHITE_SQUARES, i, l)) {
+                            downCount += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    if (downCount > 1) {
+                        dnClues[clueNumber] = [i, j, downCount];
+                    }
+                }
+                if (acrossCount > 1 || downCount > 1) {
+                    drawNumber(ctx, cellSize, clueNumber, i, j);
+                    clueNumber += 1;
+                }
+            }
+        }
+    }
+    console.log('done');
+}
+
+window.onload = function() {
+    drawGrid(AC_SQUARES, DN_SQUARES);
+}
